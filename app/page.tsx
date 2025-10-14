d7fe14cea4e9a709bd89448df1b8b5a22c1208af
Evan Tancil <evantancil@Evans-MacBook-Pro.local>
2025-10-14 19:55:50 -0400

    Initial Tailwind setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-red-500 text-white text-3xl font-bold h-screen flex items-center justify-center">
+      Tailwind is working
+    </div>
+  );
+}
